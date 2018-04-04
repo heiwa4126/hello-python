05da3a789e5707bd7bbabe7a9cdceec88777b77b
heiwa4126 <heiwa4126@gmail.com>
2018-04-04 14:57:11 +0900

    Add hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+# pylint: disable-msg=C0103, C0111
+"""
+Pythonの動作テスト
+兼テンプレート
+兼vscodeのdebugの構成作成
+"""
+
+
+def main():
+    """main"""
+    print("Hello,world!")
+
+
+if __name__ == '__main__':
+    main()
