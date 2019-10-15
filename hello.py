21d07f9f44916aa2b7e0e463ef4e897695caf72f
heiwa4126 <heiwa4126@example.com>
2019-10-15 10:46:09 +0900

    update

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # pylint: disable-msg=C0103, C0111
 """
 Pythonの動作テスト兼テンプレート
 兼vscodeのdebugの構成作成
 """
+
 
 def main():
     """main"""
@@ -12,5 +13,5 @@ def main():
     print("Good-bye cruel world!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
