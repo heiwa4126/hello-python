79400c854fcafa4fdd0d0860d407a901197b2622
heiwa4126 <heiwa4126@gmail.com>
2024-07-05 11:10:00 +0900

    update main and test

--- src/heiwa4126_hello_python/main.py
+++ src/heiwa4126_hello_python/main.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python3
-"""
-Python の動作テスト兼テンプレート
-兼 VScode の debug の構成作成
-兼 hatch のテスト
-"""
 
 
-def hello() -> None:
+def hello(msg="Python") -> None:
     """
-    This is the main function of the program.
-    It prints out various messages and tuples.
+    greeting message
     """
-    print("Hello world!")
-    print("Good-bye cruel world!")
-    print((1, 2, 3, 4))
-    print(
-        """In olden days, a glimpse of stocking
-Was looked on as something shocking.
-But now, God knows, Anything goes.
-Good authors too who once knew better words
-Now only use four-letter words
-Writing prose.
-Anything goes. """
-    )
-    print((5, 6, 7, 8))
+    print(f"Hello {msg}")
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,13 @@
 from heiwa4126_hello_python.main import hello
 
 
-def test_hello(capsys):
+def test_hello_custom_msg(capsys):
+    hello("GitHub Copilot")
+    captured = capsys.readouterr()
+    assert captured.out == "Hello GitHub Copilot\n"
+
+
+def test_hello_default_msg(capsys):
     hello()
     captured = capsys.readouterr()
-    assert (
-        captured.out
-        == "Hello world!\nGood-bye cruel world!\n(1, 2, 3, 4)\nIn olden days, a glimpse of stocking\nWas looked on as something shocking.\nBut now, God knows, Anything goes.\nGood authors too who once knew better words\nNow only use four-letter words\nWriting prose.\nAnything goes. \n(5, 6, 7, 8)\n"
-    )
+    assert captured.out == "Hello Python\n"
